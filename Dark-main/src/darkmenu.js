337ed5f91fca648458e969d03ff90b186a37bb8b
scorpion33456 <caualuque00@gmail.com>
2021-02-27 12:41:23 -0300

    First commit

---
+++ Dark-main/src/darkmenu.js
@@ -0,0 +1,39 @@
+const botmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do BOT:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *CAUUUAKKKK*
+  DUVIDAS? fdskkk
+╚════════════════════`
+}
+
+exports.botmenu = darkmenu
+
+
+
+
+
+
+
+
